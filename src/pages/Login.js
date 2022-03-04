13c8bcdaec0ab44cafc637d9ddc7851b432632f1
Clara Villa <Clara.Villa@lego.com>
2022-03-04 15:58:12 +0000

    start login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,52 @@
+import { useState } from "react";
+import axios from "axios";
+
 function Login() {
-  return <div>Login page</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const url = "http://localhost:5005/api/auth/login";
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+
+    axios
+      .post(url, { email: email, password: password })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+        setErrorMessage(error.response.data.message);
+      });
+  };
+
+  return (
+    <div>
+      Login page
+      <form onSubmit={handleLogin}>
+        <label htmlFor="email">Email Address</label>
+        <input
+          type="email"
+          value={email}
+          name="email"
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        ></input>
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          value={password}
+          name="password"
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        ></input>
+        <button type="submit">Login</button>
+      </form>
+      {errorMessage && <p className="alert alert-danger">{errorMessage}</p>}
+    </div>
+  );
 }
 
 export default Login;
